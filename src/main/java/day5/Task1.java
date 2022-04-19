35d903afa32997991c4dfcdf440502da68949738
Stayakot <pentagram.secret@gmail.com>
2022-04-19 21:09:55 +0300

    added days 5

--- src/main/java/day5/Task1.java
+++ src/main/java/day5/Task1.java
@@ -2,6 +2,62 @@ package day5;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car car1 = new Car();
+        car1.SetModel("Tarantayka");
+        car1.SetColor("Red");
+        car1.SetYear(2020);
 
+        System.out.println(car1.GetModel());
+        System.out.println(car1.GetColor());
+        System.out.println(car1.GetYear());
     }
-}
+    }
+
+ class Car {
+
+            private String Model;
+            private String Color;
+            private int Year;
+
+            public void SetModel(String CarModel) {
+                if (CarModel.isEmpty()){
+                    System.out.println("Kakaya-kakaya model'?");
+                }else {
+                Model = CarModel;}
+            }
+
+            public String GetModel() {
+                return Model;
+            }
+
+            public void SetColor(String CarColor) {
+                if (CarColor.isEmpty()){
+                    System.out.println("Kakoy-kakoy tshet?");
+                }else {
+                    Color = CarColor;}
+            }
+
+            public String GetColor() {
+                return Color;
+            }
+
+            public void SetYear(int CarYear) {
+                if (CarYear<=0){
+                    System.out.println("Skolko-skolko??");
+                }else {
+                    Year = CarYear;}
+            }
+
+            public int GetYear() {
+                return Year;
+            }
+
+        }
+
+
+
+
+
+
+
+
--- src/main/java/day5/Task2.java
+++ src/main/java/day5/Task2.java
@@ -3,6 +3,34 @@ package day5;
 
 public class Task2 {
     public static void main(String[] args) {
+        Motorbike motik = new Motorbike("Drandylet", "Siniy", 2020);
 
+        System.out.println(motik.GetModel());
+        System.out.println(motik.GetColor());
+        System.out.println(motik.GetYear());
+
+    }
+}
+class Motorbike {
+
+    private String Model;
+    private String Color;
+    private int Year;
+
+    public Motorbike(String Model, String Color, int Year){
+        this.Model=Model;
+        this.Color=Color;
+        this.Year=Year;
+
+
+    }
+    public String GetModel() {
+        return Model;
+    }
+    public String GetColor() {
+        return Color;
+    }
+    public int GetYear() {
+        return Year;
     }
 }
