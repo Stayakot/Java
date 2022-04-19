780d507e60858650f30cb57a4e37098b2e86a047
Stayakot <vladslyvka@gmail.com>
2022-04-19 18:17:32 +0300

    added days 1-4

---
+++ src/main/java/day1/Task5.java
@@ -0,0 +1,13 @@
+package day1;
+
+public class Task5 {
+    public static void main(String[] args) {
+        int i = 1980;
+        for (i =1980; i<2024; i+=4){
+            System.out.println("Олимпиада" + " "+ i +" " + "года");
+
+
+        }
+
+    }
+}
