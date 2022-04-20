5857cf4b9841ca857b54ad22748c0c50fc0caa9d
Stayakot <pentagram.secret@gmail.com>
2022-04-20 10:06:47 +0300

    added days 6

--- src/main/java/day6/Task1.java
+++ src/main/java/day6/Task1.java
@@ -2,6 +2,17 @@ package day6;
 
 public class Task1 {
     public static void main(String[] args) {
+        Car novayaMasina = new Car();
+        novayaMasina.setYear(2000);
+       int TachkaYear = novayaMasina.yearDifference(2030);
+       novayaMasina.info();
+        System.out.println(TachkaYear);
+
+        Motorbike novayaMotorbike = new Motorbike("Kravasanch","blue", 2020);
+        int BikeYear= novayaMotorbike.yearDifference(2010);
+        novayaMotorbike.info();
+        System.out.println(BikeYear);
+
 
     }
 }
--- src/main/java/day6/Task2.java
+++ src/main/java/day6/Task2.java
@@ -3,5 +3,11 @@ package day6;
 public class Task2 {
     public static void main(String[] args) {
 
+        Airplane air = new Airplane("boing", 211, 222, 233);
+        int t = air.fillUp(2020);
+        air.fillUp(2020);
+        System.out.println(air.getFuel());
+
     }
 }
+
