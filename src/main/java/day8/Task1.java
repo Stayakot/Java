599f046eeb44321d225f6cedfe95bcfdde735a54
Stayakot <pentagram.secret@gmail.com>
2022-04-20 17:15:52 +0300

    added days 8

--- src/main/java/day8/Task1.java
+++ src/main/java/day8/Task1.java
@@ -3,5 +3,29 @@ package day8;
 public class Task1 {
     public static void main(String[] args) {
 
+        String st="";
+
+//        for (int i=0; i<20000; i++){
+//            st+=i+" ";}
+//
+//            long startTime = System.currentTimeMillis();
+//            System.out.println(st);
+//            long stopTime = System.currentTimeMillis();
+//            System.out.println("Длительность работы, в мс.:" + (stopTime - startTime));
+//
+//
+
+
+
+        StringBuilder sb = new StringBuilder();
+        for ( int i=0; i<20000; i++){
+            sb.append(i +" ");}
+
+        long startTime = System.currentTimeMillis();
+        System.out.println(sb);
+        long stopTime = System.currentTimeMillis();
+        System.out.println("Длительность работы, в мс.:" + (stopTime - startTime));
     }
-}
+
+    }
+
