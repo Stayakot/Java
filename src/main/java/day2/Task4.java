5c260c19924792a46f680f9039edac4855637841
Stayakot <pentagram.secret@gmail.com>
2022-04-22 20:38:01 +0300

    update day 4

--- src/main/java/day2/Task4.java
+++ src/main/java/day2/Task4.java
@@ -5,14 +5,13 @@ public class Task4 {
     public static void main(String[] args) {
         Scanner c = new Scanner(System.in);
         double x = c.nextDouble();
-        if (x>=5){
-            double y = (x*x - 10) / (x+7);
-            System.out.println(y);
-        }else if (x>-3 && x<=4){
-            double y = (x+3)*(x*x-2);
-            System.out.println(y);
-        }else System.out.println(420.0);
-
-
+        double y = 0;
+        if (x >= 5) {
+            y = (x * x - 10) / (x + 7);
+        } else if (x > -3 && x < 4) {
+            y = (x + 3) * (x * x - 2);
+        } else y = 420.0;
+        System.out.println(y);
     }
+
 }
