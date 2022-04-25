3fd4c326a87c25221c94fb8459a9dc258d09b3ac
Stayakot <pentagram.secret@gmail.com>
2022-04-25 21:53:43 +0300

    add day 15

--- src/main/java/day15/Task1.java
+++ src/main/java/day15/Task1.java
@@ -1,7 +1,33 @@
 package day15;
 
-public class Task1 {
-    public static void main(String[] args) {
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
 
+public class Task1 {
+    public static void main(String[] args)  {
+        File file = new File("C:\\JavaMarathon2021\\src\\main\\resources\\shoes.csv");
+        PrintWriter pw = null;
+        try {
+            pw = new PrintWriter("C:\\JavaMarathon2021\\src\\main\\resources\\missing_shoes.txt");
+        } catch (FileNotFoundException e) {
+            System.out.println("\"Файл не найден\"");
+        }
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+        } catch (FileNotFoundException e) {
+            System.out.println("\"Файл не найден\"");
+        }
+        while (scanner.hasNextLine()) {
+                String line = scanner.nextLine();
+                if (line.contains(";0")){
+                    pw.println(line);
+                }
+            }pw.close();
+        }
     }
-}
+
