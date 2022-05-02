a018b1bde95da88c93ee86722e0a36593222d3be
Stayakot <pentagram.secret@gmail.com>
2022-05-02 20:04:52 +0300

    add day 17

---
+++ src/main/java/day17/ChessBoard.java
@@ -0,0 +1,23 @@
+package day17;
+
+public class ChessBoard {
+    private ChessPiece[][] board;
+
+    ChessBoard(ChessPiece[][] board) {
+        this.board = board;
+
+    }
+
+    public ChessPiece[][] getBoard() {
+        return board;
+    }
+
+    public void print(){
+        for (int i=0;i< board.length;i++) {
+            for (int j = 0; j < board.length; j++) {
+                System.out.print(board[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
--- src/main/java/day17/Task1.java
+++ src/main/java/day17/Task1.java
@@ -1,7 +1,24 @@
 package day17;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Task1 {
     public static void main(String[] args) {
+        List<ChessPiece> chess= new ArrayList<>();
+        chess.add(ChessPiece.PAWN_WHITE);
+        chess.add(ChessPiece.PAWN_WHITE);
+        chess.add(ChessPiece.PAWN_WHITE);
+        chess.add(ChessPiece.PAWN_WHITE);
+        chess.add(ChessPiece.ROOK_BLACK);
+        chess.add(ChessPiece.ROOK_BLACK);
+        chess.add(ChessPiece.ROOK_BLACK);
+        chess.add(ChessPiece.ROOK_BLACK);
+
+        for (ChessPiece chessPiece:chess ){
+            System.out.print(chessPiece.getName()+" ");
+        }
+
 
     }
 }
