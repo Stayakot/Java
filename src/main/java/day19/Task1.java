758f0d1e092cd4d5051af1fcc1ac30b46f47cacb
Stayakot <pentagram.secret@gmail.com>
2022-05-04 16:49:17 +0300

    add day 19

--- src/main/java/day19/Task1.java
+++ src/main/java/day19/Task1.java
@@ -1,10 +1,46 @@
 package day19;
 
-/**
- * @author Neil Alishev
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
 public class Task1 {
     public static void main(String[] args) {
 
+        File file = new File("C:\\JavaMarathon2021\\src\\main\\resources\\dushi.txt");
+        Scanner scanner = null;
+        try {
+            scanner = new Scanner(file);
+
+        scanner.useDelimiter("[»«<>…N„“.,:;()?!\"\\s–+]");
+        Map<String, Integer> text = new HashMap<>();
+
+        while (scanner.hasNextLine()) {
+            String line = scanner.nextLine();
+            String[] words = line.split("[»«<>…N„“.,:;()?!\"\\s–+]");
+            for (String word : words) {
+                if (!text.containsKey(word)) {
+                    text.put(word, 1);
+                } else text.put(word, text.get(word) + 1);
+            }
+        }
+        scanner.close();
+        text.remove("");
+        System.out.println("Количество Чичиков: " + text.get("Чичиков"));
+
+        text.entrySet().stream()
+                .sorted(Map.Entry.<String, Integer>comparingByValue().reversed())
+                .limit(100)
+                .forEach(System.out::println);
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
+
     }
+
 }
+
+
--- src/main/java/day19/task2/Task2.java
+++ src/main/java/day19/task2/Task2.java
@@ -1,10 +1,49 @@
 package day19.task2;
 
-/**
- * @author Neil Alishev
- */
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
 public class Task2 {
     public static void main(String[] args) {
+        File file = new File("C:\\JavaMarathon2021\\src\\main\\resources\\taxi_cars.txt");
+
+        try {
+           Scanner scanner = new Scanner(file);
+
+            Map<Integer, Point> cars = new HashMap<>();
+            Scanner numbers = new Scanner(System.in);
+            int x1 = numbers.nextInt();
+            int y1 = numbers.nextInt();
+            int x2 = numbers.nextInt();
+            int y2 = numbers.nextInt();
+            Point square1 = new Point(x1, y1);
+            Point square2 = new Point(x2, y2);
+
+
+            while (scanner.hasNextLine()) {   //Add all cars to HashMap
+
+                cars.put(scanner.nextInt(), new Point(scanner.nextInt(), scanner.nextInt()));
+
+
+            }
+            int counter = 0;
+            for (Map.Entry<Integer, Point> position : cars.entrySet()) {  //Checking cars inside squares
+
+                if (square1.getX() < position.getValue().getX() && position.getValue().getX() < square2.getX() && square1.getY() > position.getValue().getY() && position.getValue().getY() > square2.getY()) {
+                    System.out.println("Такси: " + position.getKey() + " находится в квадрате.");
+                    counter++;
+                }
+
+            }
+            System.out.println("Общее количество машин в квадрате: " + counter + ";");
+
+        } catch (FileNotFoundException e) {
+            System.out.println("Файл не найден");
+        }
 
     }
 }
