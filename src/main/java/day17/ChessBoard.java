7f272e70a72961a5c27c55f6ee08be808a5c3bd7
Stayakot <pentagram.secret@gmail.com>
2022-05-05 16:24:21 +0300

    update day 17

--- src/main/java/day17/ChessBoard.java
+++ src/main/java/day17/ChessBoard.java
@@ -1,23 +1,61 @@
 package day17;
 
+import java.util.Optional;
+
 public class ChessBoard {
-    private ChessPiece[][] board;
+//    private ChessPiece[][] board;
+//
+//    ChessBoard(ChessPiece[][] board) {
+//        this.board = board;
+//
+//    }
+//
+//    public ChessPiece[][] getBoard() {
+//        return board;
+//    }
 
-    ChessBoard(ChessPiece[][] board) {
-        this.board = board;
+    public void print(ChessPiece[][] chessPiece) {
+        for (int i = 0; i < chessPiece.length; i++) {
+            for (int j = 0; j < chessPiece[i].length; j++) {
+                chessPiece[i][j] = ChessPiece.EMPTY;
 
-    }
-
-    public ChessPiece[][] getBoard() {
-        return board;
-    }
-
-    public void print(){
-        for (int i=0;i< board.length;i++) {
-            for (int j = 0; j < board.length; j++) {
-                System.out.print(board[i][j]);
             }
-            System.out.println();
+        }
+        chessPiece[0][0]=ChessPiece.ROOK_BLACK;
+        chessPiece[0][5]=ChessPiece.ROOK_BLACK;
+        chessPiece[0][6]=ChessPiece.KING_BLACK;
+
+        chessPiece[1][1]=ChessPiece.ROOK_WHITE;
+        chessPiece[1][4]=ChessPiece.PAWN_BLACK;
+        chessPiece[1][5]=ChessPiece.PAWN_BLACK;
+        chessPiece[1][7]=ChessPiece.PAWN_BLACK;
+
+        chessPiece[2][0]=ChessPiece.PAWN_BLACK;
+        chessPiece[2][2]=ChessPiece.KNIGHT_BLACK;
+        chessPiece[2][6]=ChessPiece.PAWN_BLACK;
+
+        chessPiece[3][0]=ChessPiece.QUEEN_BLACK;
+        chessPiece[3][3]=ChessPiece.BISHOP_WHITE;
+
+        chessPiece[4][3]=ChessPiece.BISHOP_BLACK;
+        chessPiece[4][4]=ChessPiece.PAWN_WHITE;
+
+        chessPiece[5][4]=ChessPiece.BISHOP_WHITE;
+        chessPiece[5][5]=ChessPiece.PAWN_WHITE;
+
+        chessPiece[6][0]=ChessPiece.PAWN_WHITE;
+        chessPiece[6][3]=ChessPiece.QUEEN_WHITE;
+        chessPiece[6][5]=ChessPiece.PAWN_WHITE;
+        chessPiece[6][7]=ChessPiece.PAWN_WHITE;
+
+        chessPiece[7][5]=ChessPiece.ROOK_WHITE;
+        chessPiece[7][6]=ChessPiece.KING_WHITE;
+
+        for (ChessPiece[]chessPieces:chessPiece){
+            for (ChessPiece piece:chessPieces){
+                System.out.print(piece);
+            }System.out.println();
         }
     }
 }
+
